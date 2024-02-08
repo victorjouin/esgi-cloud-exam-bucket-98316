f80152afe0904def95568b8c09c06ab8da32694b
victorjouin <davidmoresta1@gmail.com>
2024-02-08 13:10:09 +0100

    gogo

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  s3: {
+    accessKeyId: 'AKIAQ7KE75KXKN5LUKGC', // TODO
+    secretAccessKey: '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', // TODO
+    endpoint: '', // TODO
+    bucket: 'esgi-cloud-exam-bucket-98316', // TODO
+  },
+  suffix: {
+    small: '_small.jpg',
+    full: '_full.jpg'
+  }
+};
